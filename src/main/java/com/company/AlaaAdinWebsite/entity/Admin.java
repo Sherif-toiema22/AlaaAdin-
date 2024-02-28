26d2c3af1f27f81d038b3d219b71b9275902748b
Sherif-toiema22 <sherif.toiema22@gmail.com>
2024-02-28 23:09:58 +0200

    Entities building

---
+++ src/main/java/com/company/AlaaAdinWebsite/entity/Admin.java
@@ -0,0 +1,38 @@
+package com.company.AlaaAdinWebsite.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+//import org.springframework.data.repository.cdi.Eager;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "admin")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String first_name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+
+
+}
---
+++ src/main/java/com/company/AlaaAdinWebsite/entity/Client.java
@@ -0,0 +1,39 @@
+package com.company.AlaaAdinWebsite.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "clients")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "Phone_number")
+    private String phone_number;
+
+    @Column(name = "address")
+    private String address;
+
+}
