6953d5206dff4b28719ca71a5d2e8d713879450f
Sherif-toiema22 <sherif.toiema22@gmail.com>
2024-02-28 19:02:28 +0200

    Initial commit

---
+++ src/main/java/com/company/AlaaAdinWebsite/AlaaAdinWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.company.AlaaAdinWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlaaAdinWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlaaAdinWebsiteApplication.class, args);
+	}
+
+}
