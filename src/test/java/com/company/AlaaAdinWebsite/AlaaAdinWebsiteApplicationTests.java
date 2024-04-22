c40e5695148fe1c8de879cb03217db016a4ae6fc
Sherif-toiema22 <sherif.toiema22@gmail.com>
2024-04-22 23:00:05 +0200

    Ahmed Salah

--- src/test/java/com/company/AlaaAdinWebsite/AlaaAdinWebsiteApplicationTests.java
+++ src/test/java/com/company/AlaaAdinWebsite/AlaaAdinWebsiteApplicationTests.java
@@ -1,13 +1,13 @@
-package com.company.AlaaAdinWebsite;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AlaaAdinWebsiteApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.company.AlaaAdinWebsite;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AlaaAdinWebsiteApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
