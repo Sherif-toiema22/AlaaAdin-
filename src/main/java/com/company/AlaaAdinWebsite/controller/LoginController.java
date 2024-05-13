7931f8ba6c5661e21e5cd93c48417ee4da9c2718
Sherif-toiema22 <sherif.toiema22@gmail.com>
2024-05-13 22:49:00 +0300

    Adding Admin login

--- src/main/java/com/company/AlaaAdinWebsite/controller/LoginController.java
+++ src/main/java/com/company/AlaaAdinWebsite/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.company.AlaaAdinWebsite.controller;
 
 import com.company.AlaaAdinWebsite.dao.ClientRepository;
 import com.company.AlaaAdinWebsite.dao.FactoryOwnerRepository;
+import com.company.AlaaAdinWebsite.entity.Admin;
 import com.company.AlaaAdinWebsite.entity.Client;
 import com.company.AlaaAdinWebsite.entity.FactoryOwner;
 import com.company.AlaaAdinWebsite.entity.LoginRequest;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 
 @RestController
@@ -21,34 +24,44 @@ public class LoginController {
 
     @PostMapping("/login")
     public Object login(@RequestBody LoginRequest loginRequest) {
-        Client client = clientRepository.findByEmail(loginRequest.getEmail());
-        FactoryOwner factoryOwner=factoryOwnerRepository.findByEmail(loginRequest.getEmail());
-        if (client != null ) {
-            if(client.getPassword().equals(loginRequest.getPassword())) {
-                return"{\"data\":{\n\"id\": "+client.getId()+",\n"+"\"firstName\": "+"\""+client.getFirstName()+"\""+",\n"+"\"lastName\": "+"\""+client.getLastName()+"\",\n"+
-                        "\"address\": "+"\""+client.getAddress()+"\",\n"+
-                        "\"email\": "+"\""+client.getEmail()+"\",\n"+
-                        "\"phone\": "+"\""+client.getPhone_number()+"\"\n"+
-                        "}\n"+",\"type\""+":\"Client\"}";
-            }else {
-                return "Right client gmail but Wrong Password";
-            }
-            } else if (factoryOwner != null) {
-            if (factoryOwner.getPassword().equals(loginRequest.getPassword())) {
-                return "{\"data\":{\n\"id\": "+factoryOwner.getId()+",\n"+"\"title\": "+"\""+factoryOwner.getTitle()+"\",\n"+"\"name\": "+"\""+factoryOwner.getName()+"\""+
-                        ",\n"+"\"address\": "+"\""+factoryOwner.getAddress()+"\",\n"+
-                        "\"email\": "+"\""+factoryOwner.getEmail()+"\""+",\n" +"\"imageLink\": "+"\""+factoryOwner.getImageLink()+"\""+ ",\n"+
-                        "\"isApproved\": "+factoryOwner.getIsApproved()+",\n"+"\"phone\": "+"\""+factoryOwner.getPhone_number()+"\""+",\n"+"\"isPublic\": "+factoryOwner.getIs_Public()+"\n"+
-                        "}\n"+",\"type\""+":\"Factory Owner\"}";
-            }else{
-                return "Right factory owner gmail but Wrong Password";
-            }
-            }
+        Admin admin = new Admin();
+        if (Objects.equals(loginRequest.getEmail(), "Admin@gmail.com")) {
+            if (Objects.equals(loginRequest.getPassword(), "3AW-BO-M&M_DB-A&S")) {
+                return "{\"data\":{\n\"id\": " + admin.getId() + ",\n" + "\"firstName\": " + "\"" + "Mahmoud" + "\"" + ",\n" + "\"lastName\": " + "\"" + "Mossad" + "\",\n" +
+                        "\"email\": " + "\"" + "Admin@gmail.com" + "\"\n" +
+                        "}\n" + ",\"type\"" + ":\"Admin\"}";
 
-//        else if ("ADMIN".equals(role)) {
-//                return "Welcome Admin!";
-//            }
-        else 
-        return "Invalid email or role not found";
+            } else {
+                return "Right Admin gmail but Wrong Password";
+            }
+        } else if (!Objects.equals(loginRequest.getEmail(), "Admin@gmail.com")) {
+            Client client = clientRepository.findByEmail(loginRequest.getEmail());
+            FactoryOwner factoryOwner = factoryOwnerRepository.findByEmail(loginRequest.getEmail());
+            if (client != null) {
+                if (client.getPassword().equals(loginRequest.getPassword())) {
+                    return "{\"data\":{\n\"id\": " + client.getId() + ",\n" + "\"firstName\": " + "\"" + client.getFirstName() + "\"" + ",\n" + "\"lastName\": " + "\"" + client.getLastName() + "\",\n" +
+                            "\"address\": " + "\"" + client.getAddress() + "\",\n" +
+                            "\"email\": " + "\"" + client.getEmail() + "\",\n" +
+                            "\"phone\": " + "\"" + client.getPhone_number() + "\"\n" +
+                            "}\n" + ",\"type\"" + ":\"Client\"}";
+                } else {
+                    return "Right client gmail but Wrong Password";
+                }
+            } else if (factoryOwner != null) {
+                if (factoryOwner.getPassword().equals(loginRequest.getPassword())) {
+                    return "{\"data\":{\n\"id\": " + factoryOwner.getId() + ",\n" + "\"title\": " + "\"" + factoryOwner.getTitle() + "\",\n" + "\"name\": " + "\"" + factoryOwner.getName() + "\"" +
+                            ",\n" + "\"address\": " + "\"" + factoryOwner.getAddress() + "\",\n" +
+                            "\"email\": " + "\"" + factoryOwner.getEmail() + "\"" + ",\n" + "\"imageLink\": " + "\"" + factoryOwner.getImageLink() + "\"" + ",\n" +
+                            "\"isApproved\": " + factoryOwner.getIsApproved() + ",\n" + "\"phone\": " + "\"" + factoryOwner.getPhone_number() + "\"" + ",\n" + "\"isPublic\": " + factoryOwner.getIs_Public() + "\n" +
+                            "}\n" + ",\"type\"" + ":\"Factory Owner\"}";
+                } else {
+                    return "Right factory owner gmail but Wrong Password";
+                }
+            }
+        } else
+            return "Invalid email or role not found";
+        return null;
     }
 }
+
+
